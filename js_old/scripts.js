40555f574e01e3854238ee1621e8a6dd5c809239
pooja yadav <sreeraj.altair@gmail.com>
2014-02-10 16:47:26 +0530

    issue with lex are fixed

--- js_old/scripts.js
+++ js_old/scripts.js
@@ -1,40 +1,35 @@
 $(document).ready(function() {
 
-
-
-// Slider  	
-	if (jQuery().flexslider) {
-	   $('.flexslider').flexslider({
-			smoothHeight: true, 
-			controlNav: false,           
-			directionNav: true,  
-			prevText: "&larr;",
-			nextText: "&rarr;",
-			selector: ".slides > .slide"
+// Slide  	
+	if (jQuery().slides) {
+	
+	    $("#slides").slides({
+	      preload: true,
+	      preloadImage: 'img/loading.gif',
+	      generateNextPrev: true,
+	      pagination: false,
+	      generatePagination: false,
+	      play: 2500,
+	      slideSpeed:'slow'
 	    });
-	};
     
-    
-    
+    };
+
 // Smooth scrolling - css-tricks.com
 	function filterPath(string){return string.replace(/^\//,'').replace(/(index|default).[a-zA-Z]{3,4}$/,'').replace(/\/$/,'');}var locationPath=filterPath(location.pathname);var scrollElem=scrollableElement('html','body');$('a[href*=#nav]').each(function(){var thisPath=filterPath(this.pathname)||locationPath;if(locationPath==thisPath&&(location.hostname==this.hostname||!this.hostname)&&this.hash.replace(/#/,'')){var $target=$(this.hash),target=this.hash;if(target){var targetOffset=$target.offset().top;$(this).click(function(event){event.preventDefault();$(scrollElem).animate({scrollTop:targetOffset},'slow',function(){location.hash=target;});});}}});function scrollableElement(els){for(var i=0,argLength=arguments.length;i<argLength;i++){var el=arguments[i],$scrollElement=$(el);if($scrollElement.scrollTop()>0){return el;}else{$scrollElement.scrollTop(1);var isScrollable=$scrollElement.scrollTop()>0;$scrollElement.scrollTop(0);if(isScrollable){return el;}}}return[];}
 
-
-
-// TOGGLES
+//TOGGLE
 	$('.toggle-view li').click(function () {
-	    var text = $(this).children('.toggle');
+	    var text = $(this).children('.t');
 	    
 	    if (text.is(':hidden')) {
 	        text.slideDown('fast');
-	        $(this).children('.toggle-title').addClass('tactive');      
+	        $(this).children('.toggle').addClass('tactive');      
 	    } else {
 	        text.slideUp('fast');
-	        $(this).children('.toggle-title').removeClass('tactive');       
+	        $(this).children('.toggle').removeClass('tactive');       
 	    }       
-	});
-	
-	
+	}); 
 		
 //TABS
 	var tabContents = $(".tab_content").hide(), 
@@ -54,52 +49,42 @@ $(document).ready(function() {
 	    return false;
 	});	
 	
-	
-	
 // OPACITY
 	$(".zoom").css({"opacity":0});
 	$(".zoom").hover(
-		function(){$(this).stop().animate({ "opacity": 0.9 }, 'slow');
-		$(this).siblings('img').stop().animate({ "opacity": 0.7 }, 'fast');},
-		
-		function(){$(this).stop().animate({ "opacity": 0 }, 'fast');
-		$(this).siblings('img').stop().animate({ "opacity": 1 }, 'fast');});
-
-
+		function(){$(this).stop().animate({ "opacity": 0.9 }, 'slow');},
+		function(){$(this).stop().animate({ "opacity": 0 }, 'fast');});
 
 // PORTFOLIO sorting	
 	// NAV 
-	$('.works-page aside menu a').click(function(){
+	$('.work_nav a').click(function(){
 		$(this).addClass("buttonactive").siblings().removeClass("buttonactive")
 	});
 	// SELECTION
 	$("#work_1").click(function() {
-	  $(".works figure").not(".work_1").stop().fadeTo("normal",0.1);
+	  $(".work").not(".work_1").stop().fadeTo("normal",0.1);
 	  $(".work_1").stop().fadeTo("normal",1);
 	});
 	
 	$("#work_2").click(function() {
-	  $(".works figure").not(".work_2").stop().fadeTo("normal",0.1);
+	  $(".work").not(".work_2").stop().fadeTo("normal",0.1);
 	  $(".work_2").stop().fadeTo("normal",1);
 	});
 	
 	$("#work_3").click(function() {
-	  $(".works figure").not(".work_3").stop().fadeTo("normal",0.1);
+	  $(".work").not(".work_3").stop().fadeTo("normal",0.1);
 	  $(".work_3").stop().fadeTo("normal",1);
 	});
 	
 	$("#work_all").click(function() {
-	  $(".works figure").stop().fadeTo("normal",1);
+	  $(".work").stop().fadeTo("normal",1);
 	});
-
-
 	
 // CONTACT form validation 	
 	if (jQuery().validate) {
 	    	$("#contact_form").validate();	 
 	};   
-	
-	
-	
+
+    
 // END
 });
