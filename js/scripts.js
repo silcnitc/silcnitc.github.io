59332aabfec7984393d5c86eed9f831d2f335ad5
nachivpn <nachivpn@gmail.com>
2014-05-13 16:07:15 +0530

    edits and back error fixed feature added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-
-
 // Slider  	
 	if (jQuery().flexslider) {
 	   $('.flexslider').flexslider({
@@ -13,13 +11,8 @@ $(document).ready(function() {
 			selector: ".slides > .slide"
 	    });
 	};
-    
-    
-    
-// Smooth scrolling - css-tricks.com
+
 	function filterPath(string){return string.replace(/^\//,'').replace(/(index|default).[a-zA-Z]{3,4}$/,'').replace(/\/$/,'');}var locationPath=filterPath(location.pathname);var scrollElem=scrollableElement('html','body');$('a[href*=#nav]').each(function(){var thisPath=filterPath(this.pathname)||locationPath;if(locationPath==thisPath&&(location.hostname==this.hostname||!this.hostname)&&this.hash.replace(/#/,'')){var $target=$(this.hash),target=this.hash;if(target){var targetOffset=$target.offset().top;$(this).click(function(event){event.preventDefault();$(scrollElem).animate({scrollTop:targetOffset},'slow',function(){location.hash=target;});});}}});function scrollableElement(els){for(var i=0,argLength=arguments.length;i<argLength;i++){var el=arguments[i],$scrollElement=$(el);if($scrollElement.scrollTop()>0){return el;}else{$scrollElement.scrollTop(1);var isScrollable=$scrollElement.scrollTop()>0;$scrollElement.scrollTop(0);if(isScrollable){return el;}}}return[];}
-
-
 
 // TOGGLES
 	$('.toggle-view li').click(function () {
